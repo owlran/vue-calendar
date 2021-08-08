6ee9d4a0adb962df1f211a16b0414c016d816607
owlran <owlran1088@gmail.com>
2021-08-09 01:16:40 +0800

    feat: support date picker mode

--- src/components/VueCalendar/index.vue
+++ src/components/VueCalendar/index.vue
@@ -1,45 +1,51 @@
 <template lang="pug">
-  .VueCalendar
-    .VueCalendar__wrapper(v-if="currentViewType === VIEW_TYPES.WEEKS_DAYS")
-      VueCalendarHeader(
-        :title="calendar.title"
-        @goPrevious="goPreMonth"
-        @goNext="goNextMonth"
-        @changeView="changeView(VIEW_TYPES.MONTH_PICKER)"
-      )
-      .VueCalendar__content
-        VueClendarDayNames(:configs="configs")
-        VueCalendarWeek(
-          v-for="(week, weekIndex) in calendar.weeks"
-          :key="weekIndex"
-          :week="week"
-          :selectedDate="selectedDate"
-          @selectDate="selectDate"
+  .VueCalendar(ref="calender" tabindex="0")
+    DatePicker(
+      v-if="isDatePickerMode"
+      :date="date"
+    )
+    .VueCalendar__wrapper(v-if="showCalendar")
+      .VueCalendar__cotent-wrapper(v-if="currentViewType === VIEW_TYPES.WEEKS_DAYS")
+        VueCalendarHeader(
+          :title="calendar.title"
+          @goPrevious="goPreMonth"
+          @goNext="goNextMonth"
+          @changeView="changeView(VIEW_TYPES.MONTH_PICKER)"
         )
-    .VueCalendar__wrapper(
-      v-else-if="currentViewType === VIEW_TYPES.YEAR_PICKER"
-    )
-      VueCalendarYearPicker(
-        :calendar="calendar"
-        :selectedDate="selectedDate"
-        @chooseYear="chooseYear"
-        @changeView="changeView"
+        .VueCalendar__content
+          VueClendarDayNames(:configs="configs")
+          VueCalendarWeek(
+            v-for="(week, weekIndex) in calendar.weeks"
+            :key="weekIndex"
+            :week="week"
+            :selectedDate="selectedDate"
+            @selectDate="selectDate"
+          )
+      .VueCalendar__content-wrapper(
+        v-else-if="currentViewType === VIEW_TYPES.YEAR_PICKER"
       )
-    .VueCalendar__wrapper(
-      v-else-if="currentViewType === VIEW_TYPES.MONTH_PICKER"
-    )
-      VueCalendarMonthPicker(
-        :calendar="calendar"
-        :selectedDate="selectedDate"
-        @chooseMonth="chooseMonth"
-        @changeView="changeView"
+        VueCalendarYearPicker(
+          :calendar="calendar"
+          :selectedDate="selectedDate"
+          @chooseYear="chooseYear"
+          @changeView="changeView"
+        )
+      .VueCalendar__content-wrapper(
+        v-else-if="currentViewType === VIEW_TYPES.MONTH_PICKER"
       )
+        VueCalendarMonthPicker(
+          :calendar="calendar"
+          :selectedDate="selectedDate"
+          @chooseMonth="chooseMonth"
+          @changeView="changeView"
+        )
 </template>
 
 <script>
 import calendarModel from '@/js/models/calendarModel';
 import VueClendarDayNames from '@/components/VueCalendar/VueCalendarDayNames.vue';
 import { VIEW_TYPES } from '@/const/index';
+import DatePicker from '@/components/DatePicker/index.vue';
 import VueCalendarHeader from './VueCalendarHeader.vue';
 import VueCalendarWeek from './VueCalendarWeek.vue';
 import VueCalendarYearPicker from './VueCalendarYearPicker.vue';
@@ -51,11 +57,17 @@ export default {
       type: String || Object,
       default: null,
     },
+    isDatePickerMode: {
+      type: Boolean,
+      default: false,
+    },
   },
   data() {
     return {
+      showCalendar: true,
       VIEW_TYPES,
       selectedDate: null,
+      popoverEl: null,
       calendarModel,
       currentCalendarDate: null,
       currentViewType: 'WEEKS_DAYS',
@@ -73,8 +85,23 @@ export default {
     VueClendarDayNames,
     VueCalendarYearPicker,
     VueCalendarMonthPicker,
+    DatePicker,
   },
   methods: {
+    onFocusIn() {
+      if (this.isDatePickerMode) {
+        this.showCalendar = true;
+      }
+    },
+    onFocusOut(e) {
+      const elContains = (el, child) => !!el && !!child && (el === child || el.contains(child));
+      if (
+        this.isDatePickerMode
+        && !elContains(this.popoverEl, e.relatedTarget)
+      ) {
+        this.showCalendar = false;
+      }
+    },
     changeView(viewType) {
       this.currentViewType = viewType;
     },
@@ -116,6 +143,10 @@ export default {
     selectDate(date) {
       this.selectedDate = date;
       this.$emit('selectDate', date);
+
+      if (this.isDatePickerMode) {
+        this.showCalendar = false;
+      }
     },
   },
   created() {
@@ -123,15 +154,26 @@ export default {
     this.initCalendar();
     this.selectedDate = this.date;
   },
+  mounted() {
+    this.showCalendar = !this.isDatePickerMode;
+
+    this.popoverEl = this.$refs.calender;
+    this.popoverEl.addEventListener('focusin', this.onFocusIn);
+    this.popoverEl.addEventListener('focusout', this.onFocusOut);
+  },
+  beforeDestroy() {
+    this.popoverEl.removeEventListener('focusin', this.onFocusIn);
+    this.popoverEl.revmoeEventListener('focusout', this.onFocusOut);
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .VueCalendar {
-  border-radius: 5px;
-  background-color: #fff;
-  padding: 20px;
-  &__content {
+  &__wrapper {
+    border-radius: 5px;
+    background-color: #fff;
+    padding: 20px;
   }
 }
 
